ca6c1b998ba15ed4ce90e3b4246ba150e771fd92
Aditya Vernekar <adityaditya711@gmail.com>
2021-11-15 11:04:09 +0530

    blogs html css done

--- modules/Blogs/main.php
+++ modules/Blogs/main.php
@@ -1,3 +1,5 @@
+<?php include("../../path.php");
+include(ROOT_PATH."/components/database/db.php");?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,11 +7,192 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>
+        AAA-Gyms- Blogs
+    </title>
+
+    <link rel="stylesheet" href="../../assets/css/navigation.css">
+    <link rel="stylesheet" href="../../assets/css/registeration.css">
+    <link rel="stylesheet" href="../../assets/css/blog.css">
+    <!-- google fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Ceviche+One&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Audiowide&family=Bungee&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/f8756ec070.js" crossorigin="anonymous"></script>
 </head>
 
 <body>
-    <h1>blogs here </h1>
+    <?php include(ROOT_PATH."/components/header.php");?>
+    <div class="page-wrapper">
+        <!-- Post Slider -->
+        <div class="post-slider">
+            <h1 class="slider-title">Trending Posts</h1>
+            <i class="fas fa-chevron-left prev"></i>
+            <i class="fas fa-chevron-right next"></i>
+
+            <div class="post-wrapper">
+
+                <div class="post">
+                    <img src="../../assets/images/bg1.jpg" alt="" class="slider-image">
+                    <div class="post-info">
+                        <h4><a href="single.php">One day your life will flash before your eyes</a></h4>
+                        <i class="far fa-user"> Author</i>
+                        &nbsp;
+                        <i class="far fa-calendar"> Mar 8, 2019</i>
+                    </div>
+                </div>
+
+                <div class="post">
+                    <img src="../../assets/images/bg1.jpg" alt="" class="slider-image">
+                    <div class="post-info">
+                        <h4><a href="single.php">One day your life will flash before your eyes</a></h4>
+                        <i class="far fa-user"> Author</i>
+                        &nbsp;
+                        <i class="far fa-calendar"> Mar 8, 2019</i>
+                    </div>
+                </div>
+
+                <div class="post">
+                    <img src="../../assets/images/bg1.jpg" alt="" class="slider-image">
+                    <div class="post-info">
+                        <h4><a href="single.php">One day your life will flash before your eyes</a></h4>
+                        <i class="far fa-user"> Author</i>
+                        &nbsp;
+                        <i class="far fa-calendar"> Mar 8, 2019</i>
+                    </div>
+                </div>
+
+                <div class="post">
+                    <img src="../../assets/images/bg1.jpg" alt="" class="slider-image">
+                    <div class="post-info">
+                        <h4><a href="single.php">One day your life will flash before your eyes</a></h4>
+                        <i class="far fa-user"> Author</i>
+                        &nbsp;
+                        <i class="far fa-calendar"> Mar 8, 2019</i>
+                    </div>
+                </div>
+
+                <div class="post">
+                    <img src="../../assets/images/bg1.jpg" alt="" class="slider-image">
+                    <div class="post-info">
+                        <h4><a href="single.php">One day your life will flash before your eyes</a></h4>
+                        <i class="far fa-user"> Author</i>
+                        &nbsp;
+                        <i class="far fa-calendar"> Mar 8, 2019</i>
+                    </div>
+                </div>
+
+
+            </div>
+
+        </div>
+        <!-- // Post Slider -->
+        <!-- Content -->
+        <div class="content clearfix">
+
+            <!-- Main Content -->
+            <div class="main-content">
+                <h1 class="recent-post-title">Recent Posts</h1>
+
+                <div class="post clearfix">
+                    <img src="../../assets/images/main1.jpg" alt="" class="post-image">
+                    <div class="post-preview">
+                        <h2><a href="single.hmtl">The strongest and sweetest songs yet remain to be sung</a></h2>
+                        <i class="far fa-user"> Author</i>
+                        &nbsp;
+                        <i class="far fa-calendar"> Mar 11, 2019</i>
+                        <p class="preview-text">
+                            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                            Exercitationem optio possimus a inventore maxime laborum.
+                        </p>
+                        <a href="single.php" class="btn read-more">Read More</a>
+                    </div>
+                </div>
+
+                <div class="post clearfix">
+                    <img src="../../assets/images/main1.jpg" alt="" class="post-image">
+                    <div class="post-preview">
+                        <h2><a href="single.hmtl">The strongest and sweetest songs yet remain to be sung</a></h2>
+                        <i class="far fa-user"> Author</i>
+                        &nbsp;
+                        <i class="far fa-calendar"> Mar 11, 2019</i>
+                        <p class="preview-text">
+                            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                            Exercitationem optio possimus a inventore maxime laborum.
+                        </p>
+                        <a href="single.php" class="btn read-more">Read More</a>
+                    </div>
+                </div>
+                <div class="post clearfix">
+                    <img src="../../assets/images/main1.jpg" alt="" class="post-image">
+                    <div class="post-preview">
+                        <h2><a href="single.hmtl">The strongest and sweetest songs yet remain to be sung</a></h2>
+                        <i class="far fa-user"> Author</i>
+                        &nbsp;
+                        <i class="far fa-calendar"> Mar 11, 2019</i>
+                        <p class="preview-text">
+                            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                            Exercitationem optio possimus a inventore maxime laborum.
+                        </p>
+                        <a href="single.php" class="btn read-more">Read More</a>
+                    </div>
+                </div>
+                <div class="post clearfix">
+                    <img src="../../assets/images/main1.jpg" alt="" class="post-image">
+                    <div class="post-preview">
+                        <h2><a href="single.hmtl">The strongest and sweetest songs yet remain to be sung</a></h2>
+                        <i class="far fa-user"> Author</i>
+                        &nbsp;
+                        <i class="far fa-calendar"> Mar 11, 2019</i>
+                        <p class="preview-text">
+                            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                            Exercitationem optio possimus a inventore maxime laborum.
+                        </p>
+                        <a href="single.php" class="btn read-more">Read More</a>
+                    </div>
+                </div>
+
+            </div>
+            <!-- // Main Content -->
+
+            <div class="sidebar">
+
+                <div class="section search">
+                    <h2 class="section-title">Search</h2>
+                    <form action="index.html" method="post">
+                        <input type="text" name="search-term" class="text-input" placeholder="Search...">
+                    </form>
+                </div>
+
+
+                <div class="section topics">
+                    <h2 class="section-title">Topics</h2>
+                    <ul>
+                        <li><a href="#">Poems</a></li>
+                        <li><a href="#">Quotes</a></li>
+                        <li><a href="#">Fiction</a></li>
+                        <li><a href="#">Biography</a></li>
+                        <li><a href="#">Motivation</a></li>
+                        <li><a href="#">Inspiration</a></li>
+                        <li><a href="#">Life Lessons</a></li>
+                    </ul>
+                </div>
+
+            </div>
+
+        </div>
+        <!-- // Content -->
+    </div>
+
+    <?php include(ROOT_PATH."/components/footer.php");?>
+
 </body>
+<!-- JQuery -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+<!-- Slick Carousel -->
+<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+
+
+<script src="../../assets/js/blog.js"></script>
 
 </html>
---
+++ modules/Blogs/single.php
@@ -0,0 +1,183 @@
+<?php include("../../path.php");
+include(ROOT_PATH."/components/database/db.php");?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>
+        AAA-Gyms- Blogs
+    </title>
+
+    <link rel="stylesheet" href="../../assets/css/navigation.css">
+    <link rel="stylesheet" href="../../assets/css/registeration.css">
+    <link rel="stylesheet" href="../../assets/css/blog.css">
+    <!-- google fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Ceviche+One&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Audiowide&family=Bungee&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/f8756ec070.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <?php include(ROOT_PATH."/components/header.php");?>
+    <!-- Page Wrapper -->
+    <div class="page-wrapper">
+
+        <!-- Content -->
+        <div class="content clearfix">
+
+            <!-- Main Content Wrapper -->
+            <div class="main-content-wrapper">
+                <div class="main-content single">
+                    <h1 class="post-title">This is the title of the Post</h1>
+
+                    <div class="post-content">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione modi error rerum possimus
+                            animi! Eos!
+                        </p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam placeat at molestias vitae!
+                            Ipsa
+                            repudiandae praesentium nobis nesciunt, iusto pariatur tenetur commodi! Iste sequi placeat
+                            dolores nulla,
+                            expedita voluptas officiis.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur, facere iste! Ex quia
+                            hic recusandae
+                            optio velit ad consectetur totam sed sunt quasi voluptates, sequi molestias alias sapiente
+                            iste asperiores
+                            nostrum est voluptatem quae earum accusantium. Totam dolorem possimus rem!</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, nisi.</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione modi error rerum possimus
+                            animi! Eos!
+                        </p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam placeat at molestias vitae!
+                            Ipsa
+                            repudiandae praesentium nobis nesciunt, iusto pariatur tenetur commodi! Iste sequi placeat
+                            dolores nulla,
+                            expedita voluptas officiis.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur, facere iste! Ex quia
+                            hic recusandae
+                            optio velit ad consectetur totam sed sunt quasi voluptates, sequi molestias alias sapiente
+                            iste asperiores
+                            nostrum est voluptatem quae earum accusantium. Totam dolorem possimus rem!</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, nisi.</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione modi error rerum possimus
+                            animi! Eos!
+                        </p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam placeat at molestias vitae!
+                            Ipsa
+                            repudiandae praesentium nobis nesciunt, iusto pariatur tenetur commodi! Iste sequi placeat
+                            dolores nulla,
+                            expedita voluptas officiis.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur, facere iste! Ex quia
+                            hic recusandae
+                            optio velit ad consectetur totam sed sunt quasi voluptates, sequi molestias alias sapiente
+                            iste asperiores
+                            nostrum est voluptatem quae earum accusantium. Totam dolorem possimus rem!</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, nisi.</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione modi error rerum possimus
+                            animi! Eos!
+                        </p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam placeat at molestias vitae!
+                            Ipsa
+                            repudiandae praesentium nobis nesciunt, iusto pariatur tenetur commodi! Iste sequi placeat
+                            dolores nulla,
+                            expedita voluptas officiis.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur, facere iste! Ex quia
+                            hic recusandae
+                            optio velit ad consectetur totam sed sunt quasi voluptates, sequi molestias alias sapiente
+                            iste asperiores
+                            nostrum est voluptatem quae earum accusantium. Totam dolorem possimus rem!</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, nisi.</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione modi error rerum possimus
+                            animi! Eos!
+                        </p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam placeat at molestias vitae!
+                            Ipsa
+                            repudiandae praesentium nobis nesciunt, iusto pariatur tenetur commodi! Iste sequi placeat
+                            dolores nulla,
+                            expedita voluptas officiis.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur, facere iste! Ex quia
+                            hic recusandae
+                            optio velit ad consectetur totam sed sunt quasi voluptates, sequi molestias alias sapiente
+                            iste asperiores
+                            nostrum est voluptatem quae earum accusantium. Totam dolorem possimus rem!</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, nisi.</p>
+                    </div>
+
+                </div>
+            </div>
+            <!-- // Main Content -->
+
+            <!-- Sidebar -->
+            <div class="sidebar single">
+
+                <div class="fb-page" data-href="https://web.facebook.com/codingpoets/" data-small-header="false"
+                    data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true">
+                    <blockquote cite="https://web.facebook.com/codingpoets/" class="fb-xfbml-parse-ignore"><a
+                            href="https://web.facebook.com/codingpoets/">Coding Poets</a></blockquote>
+                </div>
+
+
+                <div class="section popular">
+                    <h2 class="section-title">Popular</h2>
+
+                    <div class="post clearfix">
+                        <img src="../../assets/images/gym1.jpg" alt="">
+                        <a href="" class="title">
+                            <h4>How to overcome your fears</h4>
+                        </a>
+                    </div>
+                    <div class="post clearfix">
+                        <img src="../../assets/images/gym2.jpg" alt="">
+                        <a href="" class="title">
+                            <h4>How to overcome your fears</h4>
+                        </a>
+                    </div>
+                    <div class="post clearfix">
+                        <img src="../../assets/images/gym3.jpg" alt="">
+                        <a href="" class="title">
+                            <h4>How to overcome your fears</h4>
+                        </a>
+                    </div>
+                    <div class="post clearfix">
+                        <img src="../../assets/images/gym4.jpg" alt="">
+                        <a href="" class="title">
+                            <h4>How to overcome your fears</h4>
+                        </a>
+                    </div>
+                    <div class="post clearfix">
+                        <img src="../../assets/images/bg1.jpg" alt="">
+                        <a href="" class="title">
+                            <h4>How to overcome your fears</h4>
+                        </a>
+                    </div>
+
+                </div>
+
+                <div class="section topics">
+                    <h2 class="section-title">Topics</h2>
+                    <ul>
+                        <li><a href="#">Poems</a></li>
+                        <li><a href="#">Quotes</a></li>
+                        <li><a href="#">Fiction</a></li>
+                        <li><a href="#">Biography</a></li>
+                        <li><a href="#">Motivation</a></li>
+                        <li><a href="#">Inspiration</a></li>
+                        <li><a href="#">Life Lessons</a></li>
+                    </ul>
+                </div>
+            </div>
+            <!-- // Sidebar -->
+
+        </div>
+        <!-- // Content -->
+
+    </div>
+    <!-- // Page Wrapper -->
+    <?php include(ROOT_PATH."/components/footer.php");?>
+</body>
+<script src="../../assets/js/blog.js"></script>
+
+</html>
